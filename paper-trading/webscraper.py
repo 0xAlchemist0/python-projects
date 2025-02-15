601b1fe020bd724c77d43dbefe662cef0903af6f
ExoticSolutions <1exoticeditz@gmail.com>
2025-02-14 19:12:20 -0500

    updates

--- paper-trading/webscraper.py
+++ paper-trading/webscraper.py
@@ -1,4 +1,6 @@
 #we will use this scraping package to bypass cludflare
+#try to use this to wait for loaded js data
+import asyncio
 import os
 
 #for alchemy
@@ -8,12 +10,13 @@ import cloudscraper
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from playwright.async_api import Playwright, async_playwright
 
 unique_id = str(uuid.uuid4())  
 import AI
 
 load_dotenv()
-
+playwright = async_playwright()
 kolscan_leaderboard = "https://kolscan.io/leaderboard"
 kolscan_base_url = 'https://kolscan.io/'
 scraper = cloudscraper.create_scraper()
@@ -30,19 +33,32 @@ def topWallets():
 
 
 #work on tommorow
-def walletHoldings(addreses, isKolscan):
+async def walletHoldings(addreses, isKolscan):
     holdings = []
 
 
     if (isKolscan):
+        #whats left is to dissect the div with token holdings located in account_accountHoldingsContainer__3RGJ0
         for address in addreses:
-            url = kolscan_base_url + f"account/{address}"
-            request = requests.get(url)
-            top_holdings_div = BeautifulSoup(request.content, 'html.parser').find('div','account_accountHolding__q4sCD')
-            print(top_holdings_div)
-     
-
+            account_html = ""
+            soup = None
+            async with async_playwright() as playwright:
+                account_html = await html_playwright(kolscan_base_url + f"account/{address}", playwright)
+            soup = BeautifulSoup(account_html, 'html.parser')
+            print(soup.prettify())
     return None
+#we will use kolscans website to scrape and get users holdings
+#do this first i cannot stress enough
+#we will use this to extract kolscan and get the users holdings
+async def html_playwright(target_url, playwright: Playwright):
+    chromium = playwright.chromium
+    browser = await chromium.launch()
+    page = await browser.new_page()
+    await page.goto(target_url)
+    html_content = await page.content()
+    await browser.close()  
+    return html_content
+    
     
 def kolscan():
     request = requests.get(kolscan_leaderboard)
@@ -93,8 +109,9 @@ def extractElements(response, targets):
 
 # topWallets()
 
-walletHoldings({"2YJbcB9G8wePrpVBcT31o8JEed6L3abgyCjt5qkJMymV"}, True)
 
+# to run a async function we call asyncio.run(functionname(params))
+asyncio.run(walletHoldings(["2YJbcB9G8wePrpVBcT31o8JEed6L3abgyCjt5qkJMymV"], True))
 
 #schema for json:
 #name , wallet address, win rate , pnl, and wallet balance
